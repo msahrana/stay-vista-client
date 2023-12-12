866e13174d07ad35e7d8c04381387b986c0a03c3
msahrana <msahrana@gmail.com>
2023-12-13 01:43:30 +0600

    login include

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-// import Navbar from "../components/Shared/Navbar/Navbar";
+import Navbar from "../components/Shared/Navbar/Navbar";
 import Footer from "../components/Shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
-        {/* <Navbar/> */}
+        <Navbar/>
       <div className="pt-24 min-h-[calc(100vh-68px)]">
         <Outlet />
       </div>
--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -3,6 +3,8 @@ import Container from '../../components/Shared/Container'
 import { useParams } from 'react-router-dom'
 import Loader from '../../components/Shared/Loader'
 import { Helmet } from 'react-helmet-async'
+import Header from '../../components/RoomDetails/Header'
+import RoomInfo from './RoomInfo'
 
 const RoomDetails = () => {
   const { id } = useParams()
@@ -27,8 +29,12 @@ const RoomDetails = () => {
         <title>{room?.title}</title>
       </Helmet>
       <div className=''>
-        <div className='flex flex-col gap-6'>{/* Header */}</div>
-        <div className=''>{/* Room Info */}</div>
+        <div className='flex flex-col gap-6'> 
+        <Header room={room} />
+        </div>
+        <div className=''>
+          <RoomInfo room={room} />
+        </div>
         {/* Calender */}
       </div>
     </Container>
